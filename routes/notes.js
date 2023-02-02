2443e24d2d11a76871053c62598c58ce50a541b8
Big_speed90 <zulma.jeff@yahoo.com>
2023-02-01 19:14:46 -0500

    properly arranged folder, linked post and get request

--- routes/notes.js
+++ routes/notes.js
@@ -3,33 +3,49 @@ const { v4: uuidv4 } = require('uuid');
 const {
   readFromFile,
   readAndAppend,
+  writeToFile,
 } = require('../routes/helpers/fsUtils');
+//const notesData = require('../db/db.json');
 
 notes.get('/', (req, res) => {
-    readFromFile('../Develop/db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('db/db.json').then((data) => res.json(JSON.parse(data)));
+    //res.json(notesData);
+});
+
+
+
+notes.delete('/notes_id', (req, res) => {
+    let selID = parseInt(req.params.id);
+    //  Read JSON file
+    for (let i = 0; i < notesData.length; i++) {
+      if (selID === notesData[i].id) {
+        notesData.splice(i, 1);
+        let noteJSON = JSON.stringify(notesData, null, 2);
+  
+        writeToFile("db/db.json", noteJSON).then(function () {
+          console.log("Note has been deleted.");
+ 
+    });
+      }}
 });
 
 notes.post('/', (req, res) => {
+    console.log(req.body);
 
-    const { noteTitle, noteText } = req.body;
+    const { title, text } = req.body;
 
-    if (noteTitle && noteText) {
+    if (req.body) {
         const newNotes = {
-            noteTitle,
-            noteText,
+            title,
+            text,
             note_id: uuidv4(),
         };
-        readAndAppend(newNotes, '../Develop/db/db.json');
-
-        const response = {
-            status: 'Success',
-            body: newNotes,
-        };
-
-        res.json(response);
+        readAndAppend(newNotes, 'db/db.json');
+        res.json('Notes added successfully 🚀');
     }   else {
         res.json('Error in posting notes');
     }
+    //res.json(notesData);
 });
 
 module.exports = notes;
